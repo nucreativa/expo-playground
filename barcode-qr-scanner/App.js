5fa40a575169cacb6ee58eb37f2d5468e26a9d85
Ary Wibowo <ary.wibowo@brinks.com>
2023-05-17 22:11:32 +0700

    Add working Barcode and QR scanner

---
+++ barcode-qr-scanner/App.js
@@ -0,0 +1,69 @@
+import { Camera } from "expo-camera";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function App() {
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [barcode, setBarcode] = useState("");
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function readBarcode(a) {
+    setBarcode(a.data);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} onBarCodeScanned={readBarcode}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.text}>{barcode}</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  camera: {
+    width: 300,
+    height: 400,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
